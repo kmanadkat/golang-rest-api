70c32f72f493b7bc117b5c82a9d25dbe86c8240b
kmanadkat <kmanadkat@gmail.com>
2024-10-18 10:58:42 +0530

    feat: Update Todo

--- main.go
+++ main.go
@@ -103,7 +103,35 @@ func createTodo(c *fiber.Ctx) error {
 }
 
 func updateTodo(c *fiber.Ctx) error {
-	return nil
+	// Prepare Object Id
+	id := c.Params("id")
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid Todo Id"})
+	}
+
+	// Prepare Todo
+	todo := &Todo{}
+	todo.ID = objectId
+	err = c.BodyParser(todo)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"msg": "Invalid request"})
+	}
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"msg": "Todo body is required"})
+	}
+
+	// Update Todo
+	filter := bson.M{"_id": objectId}
+	update := bson.M{"$set": bson.M{"body": todo.Body, "completed": todo.Completed}}
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"msg": "Error updating todo"})
+	}
+
+	// Respond Back
+	response := fiber.Map{"msg": "Todo updated successfully", "data": todo}
+	return c.Status(200).JSON(response)
 }
 
 func deleteTodo(c *fiber.Ctx) error {
